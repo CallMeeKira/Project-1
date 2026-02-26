ca023633c857f67da5fd0e7c9f7928cc64e3cbcb
CallMeeKira <164614026+CallMeeKira@users.noreply.github.com>
2026-02-26 19:22:54 +0100

    TEST COMMIT + Added Lunite Ingot and Raw Lunite

---
+++ src/main/java/akira/strayechoes/item/ModItems.java
@@ -0,0 +1,27 @@
+package akira.strayechoes.item;
+
+import akira.strayechoes.EchoesOfTheStray;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item LUNITE_INGOT = registerItem("lunite_ingot", new Item(new Item.Settings()));
+    public static final Item RAW_LUNITE = registerItem("raw_lunite", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(EchoesOfTheStray.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        EchoesOfTheStray.LOGGER.info("Registering Mod Items for " + EchoesOfTheStray.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(LUNITE_INGOT);
+            entries.add(RAW_LUNITE);
+        });
+    }
+}
