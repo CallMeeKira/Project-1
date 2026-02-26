19f7726bd4ae9a21b79bf2360f3670de53769c18
CallMeeKira <164614026+CallMeeKira@users.noreply.github.com>
2026-02-26 17:35:46 +0100

    Initial commit

---
+++ src/main/java/akira/strayechoes/EchoesOfTheStrayClient.java
@@ -0,0 +1,10 @@
+package akira.strayechoes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EchoesOfTheStrayClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
