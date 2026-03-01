78f9a81ff4ca585941ffd387eca12a0e44cc9217
Milano <milanohoogvliets@hotmail.com>
2026-03-01 19:43:31 +0100

    + Helmet for lunite and wearable

--- src/main/java/akira/strayechoes/item/ModItems.java
+++ src/main/java/akira/strayechoes/item/ModItems.java
@@ -2,6 +2,7 @@ package akira.strayechoes.item;
 
 import akira.strayechoes.EchoesOfTheStray;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -12,6 +13,8 @@ public class ModItems {
     public static final Item LUNITE_INGOT = registerItem("lunite_ingot", new Item(new Item.Settings()));
     public static final Item RAW_LUNITE = registerItem("raw_lunite", new Item(new Item.Settings()));
     public static final Item LUNITE_NUGGET = registerItem("lunite_nugget", new Item(new Item.Settings()));
+    public static final Item LUNITE_HELMET = registerItem("lunite_helmet", new ArmorItem(ModArmorMaterials.LUNITE, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(ArmorItem.Type.HELMET.getMaxDamage(15)))
+    );
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(EchoesOfTheStray.MOD_ID, name), item);
